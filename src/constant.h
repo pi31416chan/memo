e4695ade26f09051445c2fb7681d02fc6e498118
XW Chan <xingwei.chan@outlook.com>
2025-06-08 21:15:40 +0800

    Fixed MEMO_DATAFILE path issue

--- src/constant.h
+++ src/constant.h
@@ -9,9 +9,9 @@ using namespace std;
 namespace fs = std::filesystem;
 using path = fs::path;
 
-const string LOCAL_DIRNAME = ".local";
+const string LOCAL_DIRNAME = ".local/share";
 const string MEMO_HOME_NAME = "memo";
-const string MEMO_DATAFILE_NAME = "/data.json";
+const string MEMO_DATAFILE_NAME = "data.json";
 const fs::path USER_HOME = string(getenv("HOME"));
 const fs::path MEMO_HOME = USER_HOME / LOCAL_DIRNAME / MEMO_HOME_NAME;
 const fs::path MEMO_DATAFILE = MEMO_HOME / MEMO_DATAFILE_NAME;
